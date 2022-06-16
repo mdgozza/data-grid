0ad96528bbc99bdc5e39986b7cdacbd99283fa56
mdgozza <mdgozza@gmail.com>
2022-06-16 12:34:02 -0400

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+          </tr>
+          <tr>
+            <th>Last Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Marcello</td>
+          </tr>
+          <tr>
+            <td>Gozza</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-xhna5q
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-xhna5q)
